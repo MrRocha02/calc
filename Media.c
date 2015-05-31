fef27e3d355b31179e2bb837e4e27c8610e0c03c
Maike Rony <maike_rony@hotmail.com>
2015-05-31 15:09:20 -0300

    Create Media.c

---
+++ Media.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main () {
+ 	
+ 	float notas,media;
+ 	int i;
+ 	
+ 	for (i=0;i<4;i++) {
+ 		printf("Digite %d Nota: ",i+1);
+ 		scanf("%f",&notas);
+ 		media=notas+media;
+	 }
+ 	
+ 	media=media/i;
+
+ 	if ((media<6) && (media>=0))
+ 		printf("\nMedia: %.2f \nReprovado!",media);
+ 	
+ 	else if ((media>=6) && (media <=10))
+ 		printf("\nMedia: %.2f \nReprovado!",media);
+ 	
+ 	else
+ 		printf("\nMedia: %.2f \nMedia Invalida",media);
+ }
