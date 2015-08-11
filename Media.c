c1050256c85b02e5562096342ec0624718dc1b34
Luiz Fernando <luiz_1994@hotmail.com.br>
2015-08-11 14:30:53 -0300

    Revert "MinhaPrimeiraModific..."
    This reverts commit 0337001c24ee32c659a336b4c1c8cc053a612620.

--- Media.c
+++ Media.c
@@ -1,27 +1,25 @@
 #include<stdio.h>
-
-//A única biblioteca necessária para esse programa é essa acima...
-
-
-int main ()
-{
+#include<conio.h>
+// código fodidão kkkkk
+ int main () {
+ 	
  	float notas,media;
  	int i;
+ 	
+ 	for (i=0;i<4;i++) {
+ 		printf("Digite %d Nota: ",i+1);
+ 		scanf("%f",&notas);
+ 		media=notas+media;
+	 }
+ 	
+ 	media=media/i;
 
- 	for (i = 0; i < 4; i++)
-    {
- 		printf("Digite a nota do teste %d: ",i+1);
- 		scanf("%f", &notas);
- 		media = notas + media;
-    }
-
- 	media = media/i;
-
- 	if ((media <6) && (media>=0))printf("\n Media: %.2f \n Reprovado! \n", media);
- 	else if ((media >= 6) && (media <=10)) printf("\n Media: %.2f \n Aprovado! \n", media);
-    else printf("Nota inválida. \n");
-
-
-    return 0;
-}
-//
+ 	if ((media<6) && (media>=0))
+ 		printf("\nMedia: %.2f \nReprovado!",media);
+ 	
+ 	else if ((media>=6) && (media <=10))
+ 		printf("\nMedia: %.2f \nReprovado!",media);
+ 	
+ 	else
+ 		printf("\nMedia: %.2f \nMedia Invalida",media);
+ }
