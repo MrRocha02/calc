0337001c24ee32c659a336b4c1c8cc053a612620
Luiz Fernando <luiz_1994@hotmail.com.br>
2015-08-11 14:29:06 -0300

    MinhaPrimeiraModific...

--- Media.c
+++ Media.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
-#include<conio.h>
-// código fodidão kkkkk
- int main () {
- 	
+
+//A única biblioteca necessária para esse programa é essa acima...
+
+
+int main ()
+{
  	float notas,media;
  	int i;
- 	
- 	for (i=0;i<4;i++) {
- 		printf("Digite %d Nota: ",i+1);
- 		scanf("%f",&notas);
- 		media=notas+media;
-	 }
- 	
- 	media=media/i;
 
- 	if ((media<6) && (media>=0))
- 		printf("\nMedia: %.2f \nReprovado!",media);
- 	
- 	else if ((media>=6) && (media <=10))
- 		printf("\nMedia: %.2f \nReprovado!",media);
- 	
- 	else
- 		printf("\nMedia: %.2f \nMedia Invalida",media);
- }
+ 	for (i = 0; i < 4; i++)
+    {
+ 		printf("Digite a nota do teste %d: ",i+1);
+ 		scanf("%f", &notas);
+ 		media = notas + media;
+    }
+
+ 	media = media/i;
+
+ 	if ((media <6) && (media>=0))printf("\n Media: %.2f \n Reprovado! \n", media);
+ 	else if ((media >= 6) && (media <=10)) printf("\n Media: %.2f \n Aprovado! \n", media);
+    else printf("Nota inválida. \n");
+
+
+    return 0;
+}
+//
